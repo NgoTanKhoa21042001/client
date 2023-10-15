c40cb3fd51a14cd1f22ed0f86151041010cac8c7
NgoTanKhoa21042001 <kakashi21042001@gmail.com>
2023-10-15 17:14:58 +0700

    Creating Login page

---
+++ src/components/Login-Page/Avatar.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { AvatarProps } from "@/type";
+import Image from "next/image";
+import React from "react";
+
+const Avatar = ({ avatarId, setAvatarId }: AvatarProps) => {
+  return (
+    <div
+      className="avatar cursor-pointer mx-auto mb-5 tooltip"
+      data-tip="Click to generate avatar."
+      onClick={() => setAvatarId((Math.random() * 20).toFixed())}
+    >
+      <div className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+        <Image
+          src={`https://robohash.org/${avatarId}.png`}
+          width={256}
+          height={256}
+          alt="avatar"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Avatar;
